a38d614ceaff0ebe4a45e29eb9bc9f8000f5c02e
Shoichiro27 <johnsnow27@icloud.com>
2025-07-06 15:04:16 +0900

    kadai_018を追加

---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,13 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18 {
+	public void setGivenName() {
+		String setGivenName = "太郎";
+		System.out.println("名前は" + this.familyName + setGivenName + "です");
+		System.out.println("住所は" + this.address + "です");
+	}
+	
+	void eachIntroduce() {
+		System.out.println("私はJavaが得意です");
+	}
+}
